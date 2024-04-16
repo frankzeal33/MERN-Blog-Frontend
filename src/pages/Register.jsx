a155c3199e16fea1961589e0ad6e69d19c997cc0
frankzeal33 <93032385+frankzeal33@users.noreply.github.com>
2024-04-16 22:07:50 +0100

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Loader from '../Loader';
+import Loader from '../components/Loader';
 
 const Register = () => {
   const [userData, setUserData] = useState({
